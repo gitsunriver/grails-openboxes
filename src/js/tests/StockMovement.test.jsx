c38b66197f4eefbd326b8a6e572d485ea58e2da4
gitsunriver <superdeveloper0217@gmail.com>
2018-05-21 19:13:33 +0200

    OBPIH-855: Add wrapper for FieldArray and button field

--- src/js/tests/StockMovement.test.jsx
+++ src/js/tests/StockMovement.test.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import StockMovement from '../components/stock-movement-wizard/StockMovement';
+import StockMovement from '../components/StockMovement';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 test('StockMovement component is rendering properly', () => {
   const wrapper = shallow(<StockMovement />);
 
-  expect(wrapper.text()).toEqual('<ReduxForm />');
-  // TODO add more tests once we add more steps to Stock Movement
+  expect(wrapper.find('#mainApp')).toHaveLength(1);
+
+  expect(wrapper.text()).toEqual('React Component');
 });
