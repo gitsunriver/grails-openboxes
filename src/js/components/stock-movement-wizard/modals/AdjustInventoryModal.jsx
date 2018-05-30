df7791858a9c4fea14c17b37ac06a3f6b68c14a2
gitsunriver <superdeveloper0217@gmail.com>
2018-05-30 19:34:42 +0200

    OBPIH-872: Create step 2 form (movement from a vendor)

--- src/js/components/stock-movement-wizard/modals/AdjustInventoryModal.jsx
+++ src/js/components/stock-movement-wizard/modals/AdjustInventoryModal.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import ModalWrapper from '../../form-elements/ModalWrapper';
 
-const AdjustInventoryModal = (props) => {
-  const {
-    fieldConfig: { attributes, getDynamicAttr },
-  } = props;
-  const dynamicAttr = getDynamicAttr ? getDynamicAttr(props) : {};
-  const attr = { ...attributes, ...dynamicAttr };
-
-  return (
-    <ModalWrapper {...attr}>
-      <div>Adjust Inventory form placeholder</div>
-    </ModalWrapper>
-  );
-};
+const AdjustInventoryModal = () => (
+  <ModalWrapper title="Adjust Inventory" >
+    <div>Adjust Inventory form placeholder</div>
+  </ModalWrapper>
+);
 
 export default AdjustInventoryModal;
-
-AdjustInventoryModal.propTypes = {
-  fieldName: PropTypes.string.isRequired,
-  fieldConfig: PropTypes.shape({
-    getDynamicAttr: PropTypes.func,
-  }).isRequired,
-};
