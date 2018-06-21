6a88047af053a4a42488efe5232aab3b50db3722
gitsunriver <superdeveloper0217@gmail.com>
2018-06-21 20:06:49 +0200

    OBPIH-952: Fetch products and recipients form API on Step 2

--- src/js/components/form-elements/TableRowWithSubfields.jsx
+++ src/js/components/form-elements/TableRowWithSubfields.jsx
@@ -1,17 +1,32 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FieldArray } from 'redux-form';
 
 import TableBody from './TableBody';
-import TableRow from './TableRow';
+import { renderFormField } from '../../utils/form-utils';
 
 const TableRowWithSubfields = (props) => {
   const {
-    fieldsConfig, index, field, properties,
+    fieldsConfig, index, field, addRow, properties, removeRow,
   } = props;
 
+  const dynamicAttr = fieldsConfig.getDynamicRowAttr ? fieldsConfig.getDynamicRowAttr(props) : {};
+
   return [
-    <TableRow key={field} {...props} />,
+    <tr key={field} {...dynamicAttr}>
+      { _.map(fieldsConfig.fields, (config, name) => (
+        <td key={`${field}.${name}`} className="align-middle">
+          { !config.subfield && renderFormField(config, `${field}.${name}`, {
+            ...properties,
+            arrayField: true,
+            addRow,
+            removeRow,
+            rowIndex: index,
+          })}
+        </td>
+      ))}
+    </tr>,
     <FieldArray
       key={`${field}.${fieldsConfig.subfieldKey}`}
       name={`${field}.${fieldsConfig.subfieldKey}`}
--- src/js/mockedData.jsx
+++ src/js/mockedData.jsx
@@ -66,20 +66,6 @@ const USERNAMES_MOCKS = ['Julian Benson',
   'Kim Dennis',
 ];
 
-const STOCK_LIST_MOCKS = [
-  { value: '1', label: '1st Stock List' },
-  { value: '2', label: '2nd Stock List' },
-  { value: '3', label: '3rd Stock List' },
-  { value: '4', label: '4th Stock List' },
-];
-
-const PRODUCTS_MOCKS = [
-  { value: { code: 1, name: 'Advil 200mg' }, label: 'Advil 200mg' },
-  { value: { code: 2, name: 'Tylenol 325mg' }, label: 'Tylenol 325mg' },
-  { value: { code: 3, name: 'Aspirin 20mg' }, label: 'Aspirin 20mg' },
-  { value: { code: 4, name: 'Similac Advance low iron 400g' }, label: 'Similac Advance low iron 400g' },
-];
-
 const STOCK_LIST_ITEMS_MOCKS = {
   1: [
     {
@@ -530,8 +516,6 @@ const RECEIPT_MOCKS = {
 export {
   LOCATION_MOCKS,
   USERNAMES_MOCKS,
-  STOCK_LIST_MOCKS,
-  PRODUCTS_MOCKS,
   STOCK_LIST_ITEMS_MOCKS,
   REASON_CODE_MOCKS,
   AVAILABLE_LOTS,
