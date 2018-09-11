c0e70f66f859b4442eaf399345809cb6a7fbb543
gitsunriver <superdeveloper0217@gmail.com>
2018-09-11 01:38:51 -0500

    OBPIH-1063 Fixed several issues with behavior related to quantity revised property

--- grails-app/domain/org/pih/warehouse/shipping/Shipment.groovy
+++ grails-app/domain/org/pih/warehouse/shipping/Shipment.groovy
@@ -286,10 +286,6 @@ class Shipment implements Comparable, Serializable {
 	Boolean wasReceived() { 
 		return events.any { it.eventType?.eventCode == EventCode.RECEIVED }
 	}
-
-	Boolean wasPartiallyReceived() {
-		return events.any { it.eventType?.eventCode == EventCode.PARTIALLY_RECEIVED }
-	}
 	
 	/*
 	Boolean isIncoming(Location currentLocation) { 
@@ -369,11 +365,6 @@ class Shipment implements Comparable, Serializable {
 			                             date:this.getActualDeliveryDate(),
 			                             location:this.destination] )
 		}
-        else if (wasPartiallyReceived()) {
-            return new ShipmentStatus( [ code:ShipmentStatusCode.PARTIALLY_RECEIVED,
-                                         date:this.getActualDeliveryDate(),
-                                         location:this.destination] )
-        }
 		else if (this.hasShipped()) {
 			return new ShipmentStatus( [ code:ShipmentStatusCode.SHIPPED,
 			                             date:this.getActualShippingDate(),
