177c969c128c2644c31f0628521141b0f79b921c
gitsunriver <superdeveloper0217@gmail.com>
2018-09-25 19:08:12 +0200

    Saving edits when go back and add stock list

--- src/js/fr.js
+++ src/js/fr.js
@@ -2,53 +2,307 @@ const fr = {
   navbar: {
     dashboard: {
       label: 'Tableau de bord',
-      link: '/openboxes/dashboard/index',
+    },
+    analytics: {
+      label: 'Analytics',
+      subsections: {
+        inventoryBrowser: {
+          label: 'Parcourir l\'inventaire',
+        },
+        snapshot: {
+          label: 'Inventory Snapshots',
+        },
+      },
+    },
+    inventory: {
+      label: 'Inventaire',
+      subsections: {
+        browse: {
+          label: 'Parcourir l\'inventaire',
+        },
+        manage: {
+          label: 'Gérer línventaire',
+        },
+        browseByCategory: {
+          label: 'Parcourir par catégorie',
+        },
+        equipment: {
+          label: 'Equipment',
+        },
+        medicines: {
+          label: 'Medicines',
+        },
+        other: {
+          label: 'Other',
+        },
+        perishables: {
+          label: 'Perishables',
+        },
+        supplies: {
+          label: 'Supplies',
+        },
+      },
+    },
+    purchaseOrders: {
+      label: 'Commandes',
+      subsections: {
+        purchaseOrdersWorkflow: {
+          label: 'Ajouter commande entrante',
+        },
+        order: {
+          label: 'Commandes',
+        },
+      },
+    },
+    requisitions: {
+      label: 'Demandes',
+      subsections: {
+        stockRequisition: {
+          label: 'Create stock requisition',
+        },
+        nonStockRequisition: {
+          label: 'Create non-stock requisition',
+        },
+        adHocRequisition: {
+          label: 'Create adhoc stock requisition',
+        },
+        requisitionList: {
+          label: 'Demandes',
+        },
+        requisitionsAll: {
+          label: 'Tout',
+        },
+      },
+    },
+    shipping: {
+      label: 'Livraisons',
+      subsections: {
+        createShipment: {
+          label: 'Ajouter une livraison sortante',
+        },
+        shipmentList: {
+          label: 'Livraisons sortantes',
+        },
+        shipmentAll: {
+          label: 'Tout',
+        },
+      },
     },
     stockMovement: {
       label: 'Stock Movement',
-      link: '#',
       subsections: {
-        listStockMovements: {
-          label: 'List Stock Movements',
-          link: '/openboxes/stockMovement/list',
-        },
         creteStockMovement: {
           label: 'Create Stock Movement',
-          link: '/openboxes/stockMovement/index',
+        },
+        putAway: {
+          label: 'Put Away',
+          link: '/openboxes/putAway/index',
         },
       },
     },
     receiving: {
       label: 'Reception',
-      link: '#',
       subsections: {
-        listPutAways: {
-          label: 'List Put Aways',
-          link: '/openboxes/putAway/list',
+        createShipment: {
+          label: 'Ajouter une livraison entrante',
         },
-        createPutAway: {
-          label: 'Create Put Away',
-          link: '/openboxes/putAway/index',
+        shipmentList: {
+          label: 'Livraisons entrantes',
+        },
+        shipmentAll: {
+          label: 'Tout',
+        },
+        pending: {
+          label: 'En cours',
+        },
+      },
+    },
+    reporting: {
+      label: 'Rapport',
+      subsections: {
+        showBinLocationReport: {
+          label: 'Bin Location Report',
+        },
+        inventory: {
+          label: 'Baseline QoH Report',
+        },
+        showTransactionReport: {
+          label: 'Afficher rappaport d\'inventaire',
+        },
+        consumption: {
+          label: 'Consumption Report',
+        },
+        listDailyTransactions: {
+          label: 'Transactions journalières',
+        },
+        showShippingReport: {
+          label: 'Afficher rappaport de livraisons',
+        },
+        showInventorySamplingReport: {
+          label: 'Inventory Sampling Report',
+        },
+        listExpiredStock: {
+          label: 'Stock expiré',
+        },
+        listExpiringStock: {
+          label: 'Stock proche d\'expiration',
+        },
+        listLowStock: {
+          label: 'Stock bas',
+        },
+        listReorderStock: {
+          label: 'Reorder stock',
+        },
+        exportBinLocation: {
+          label: 'Export bin locations',
+        },
+        exportAsCsv: {
+          label: 'Export products',
+        },
+        exportLatestInventoryDate: {
+          label: 'Export latest inventory date',
+        },
+        inventoryLevelExport: {
+          label: 'Export inventory levels',
+        },
+        requisitionExport: {
+          label: 'Export requisitions',
+        },
+        requisitionItem: {
+          label: 'Export requisition intems',
         },
       },
     },
     products: {
       label: 'Produits',
-      link: '#',
       subsections: {
         product: {
           label: 'Produits',
-          link: '/openboxes/product/list',
+        },
+        productGroup: {
+          label: 'Generic Products',
+        },
+        productSupplier: {
+          label: 'Products Suppliers',
+        },
+        productAssociation: {
+          label: 'Associations',
+        },
+        productCatalog: {
+          label: 'Catalogs',
+        },
+        productComponent: {
+          label: 'Components',
+        },
+        attribute: {
+          label: 'Attributes',
+        },
+        category: {
+          label: 'Categories',
+        },
+        tag: {
+          label: 'Tags',
+        },
+        unitOfMeasure: {
+          label: 'Unit of Measure',
+        },
+        unitOfMeasureClass: {
+          label: 'UoM Class',
+        },
+        inventoryLevel: {
+          label: 'Inventory Levels',
+        },
+        productCreate: {
+          label: 'Create new product',
+        },
+        productBatchEdit: {
+          label: 'Batch edit product',
+        },
+        productImportAsCsv: {
+          label: 'import products',
+        },
+        productExportAsCsv: {
+          label: 'Export products',
         },
       },
     },
     configuration: {
       label: 'Configuration',
-      link: '#',
       subsections: {
+        showSettings: {
+          label: 'Settings',
+        },
+        migration: {
+          label: 'Migrate Data',
+        },
+        console: {
+          label: 'Console',
+        },
+        cache: {
+          label: 'Cache',
+        },
+        clickstream: {
+          label: 'Clickstream',
+        },
+        sendMail: {
+          label: 'Email',
+        },
+        localization: {
+          label: 'Localization',
+        },
+        documentType: {
+          label: 'Document Types',
+        },
+        eventType: {
+          label: 'Event Types',
+        },
+        locationGroup: {
+          label: 'Location groups',
+        },
+        locationType: {
+          label: 'Location types',
+        },
+        partyType: {
+          label: 'Party types',
+        },
+        partyRole: {
+          label: 'Party roles',
+        },
+        location: {
+          label: 'Locations',
+        },
+        shipper: {
+          label: 'Expéditeurs',
+        },
+        organization: {
+          label: 'Organizations',
+        },
+        shipmentWorkflow: {
+          label: 'Shipment Workflows',
+        },
+        document: {
+          label: 'Documents',
+        },
         person: {
           label: 'Tous les individus',
-          link: '/openboxes/person/list',
+        },
+        requisitionTemplate: {
+          label: 'Stock lists',
+        },
+        listAllTransactions: {
+          label: 'Transactions',
+        },
+        user: {
+          label: 'Utilisateurs',
+        },
+        editTransaction: {
+          label: 'Ajouter une transaction',
+        },
+        importInventory: {
+          label: 'Importer Inventaire',
+        },
+        importInventoryLevel: {
+          label: 'Importer Niveau des stocks',
         },
       },
     },
