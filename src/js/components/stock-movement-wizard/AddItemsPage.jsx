251f9a9d266867d6349a3bf6166b0a51e642d911
gitsunriver <superdeveloper0217@gmail.com>
2018-10-16 11:29:17 +0200

    OBPIH-1209 Fix autopick

--- src/js/components/stock-movement-wizard/AddItemsPage.jsx
+++ src/js/components/stock-movement-wizard/AddItemsPage.jsx
@@ -630,6 +630,7 @@ class AddItemsPage extends Component {
    * @public
    */
   removeAll() {
+    this.fetchAndSetLineItems();
     const removeItemsUrl = `/openboxes/api/stockMovements/${this.state.values.stockMovementId}`;
     const payload = {
       id: this.state.values.stockMovementId,
@@ -638,10 +639,10 @@ class AddItemsPage extends Component {
         delete: 'true',
       })),
     };
+    this.fetchAndSetLineItems();
 
     return apiClient.post(removeItemsUrl, payload)
       .catch(() => {
-        this.fetchAndSetLineItems();
         this.props.hideSpinner();
         return Promise.reject(new Error('Could not delete requisition items'));
       });
@@ -764,7 +765,7 @@ class AddItemsPage extends Component {
               <button
                 type="button"
                 disabled={invalid}
-                onClick={() => this.removeAll().then(() => this.fetchAndSetLineItems())}
+                onClick={() => this.removeAll()}
                 className="float-right py-1 mb-1 btn btn-outline-danger align-self-end"
               >
                 <span><i className="fa fa-remove pr-2" />Delete all</span>
