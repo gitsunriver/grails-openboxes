08def54c1d273f2bc12f453b3c27948fb4b76509
gitsunriver <superdeveloper0217@gmail.com>
2019-01-14 12:20:49 +0100

    OBPIH-1260 Add current bin to put away report

--- src/js/components/stock-movement-wizard/AddItemsPage.jsx
+++ src/js/components/stock-movement-wizard/AddItemsPage.jsx
@@ -226,7 +226,6 @@ const VENDOR_FIELDS = {
         type: SelectField,
         label: 'stockMovement.item.label',
         flexWidth: '4',
-        required: true,
         attributes: {
           className: 'text-left',
           async: true,
@@ -253,14 +252,12 @@ const VENDOR_FIELDS = {
         attributes: {
           dateFormat: 'MM/DD/YYYY',
           autoComplete: 'off',
-          placeholderText: 'MM/DD/YYYY',
         },
       },
       quantityRequested: {
         type: TextField,
         label: 'stockMovement.quantity.label',
         flexWidth: '1',
-        required: true,
         attributes: {
           type: 'number',
         },
