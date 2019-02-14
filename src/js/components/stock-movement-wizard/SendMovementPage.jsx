38b0a1f68b93ba03403b4e26a288774d19a26b2e
gitsunriver <superdeveloper0217@gmail.com>
2019-02-14 10:16:20 +0100

    OBPIH-1739 Add funcionality to sort putaway items

--- src/js/components/stock-movement-wizard/SendMovementPage.jsx
+++ src/js/components/stock-movement-wizard/SendMovementPage.jsx
@@ -7,7 +7,6 @@ import Alert from 'react-s-alert';
 import { Form } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 import { getTranslate } from 'react-localize-redux';
-import { confirmAlert } from 'react-confirm-alert';
 
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
@@ -195,19 +194,6 @@ class SendMovementPage extends Component {
   onSave(values) {
     this.props.showSpinner();
 
-    this.saveValues(values)
-      .then(() => {
-        this.props.hideSpinner();
-
-        if (values.statusCode === 'ISSUED') {
-          this.fetchStockMovementData();
-        }
-        Alert.success(this.props.translate('alert.saveSuccess.label', 'Changes saved successfully'));
-      })
-      .catch(() => this.props.hideSpinner());
-  }
-
-  saveValues(values) {
     let payload = {
       dateShipped: values.dateShipped,
       'shipmentType.id': values.shipmentType,
@@ -224,7 +210,16 @@ class SendMovementPage extends Component {
       };
     }
 
-    return this.saveShipment(payload);
+    this.saveShipment(payload)
+      .then(() => {
+        this.props.hideSpinner();
+
+        if (values.statusCode === 'ISSUED') {
+          this.fetchStockMovementData();
+        }
+        Alert.success(this.props.translate('alert.saveSuccess.label', 'Changes saved successfully'));
+      })
+      .catch(() => this.props.hideSpinner());
   }
 
   /**
@@ -387,29 +382,9 @@ class SendMovementPage extends Component {
     }
   }
 
-  previousPage(values) {
-    const errors = validate(values);
-    if (_.isEmpty(errors)) {
-      this.saveValues(values)
-        .then(() => this.props.previousPage(values));
-    } else {
-      confirmAlert({
-        title: this.props.translate('confirmPreviousPage.label', 'Validation error'),
-        message: this.props.translate('confirmPreviousPage.message.label', 'Cannot save due to validation error on page'),
-        buttons: [
-          {
-            label: this.props.translate('confirmPreviousPage.correctError.label', 'Correct error'),
-          },
-          {
-            label: this.props.translate('confirmPreviousPage.continue.label ', 'Continue (lose unsaved work)'),
-            onClick: () => this.props.previousPage(values),
-          },
-        ],
-      });
-    }
-  }
-
   render() {
+    const { previousPage } = this.props;
+
     return (
       <div>
         <hr />
@@ -488,7 +463,7 @@ class SendMovementPage extends Component {
                   type="button"
                   className="btn btn-outline-primary btn-form btn-xs"
                   disabled={values.statusCode === 'ISSUED'}
-                  onClick={() => this.previousPage(values)}
+                  onClick={() => previousPage(values)}
                 >
                   <Translate id="default.button.previous.label" defaultMessage="Previous" />
                 </button>
@@ -552,7 +527,7 @@ class SendMovementPage extends Component {
                   type="button"
                   className="btn btn-outline-primary btn-form btn-xs"
                   disabled={values.statusCode === 'ISSUED'}
-                  onClick={() => this.previousPage(values)}
+                  onClick={() => previousPage(values)}
                 > <Translate id="default.button.previous.label" defaultMessage="Previous" />
                 </button>
                 <button
