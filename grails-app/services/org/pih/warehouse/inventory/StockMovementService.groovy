da4b3c293b331f94ec080c1755cbe36fe3591ee7
gitsunriver <superdeveloper0217@gmail.com>
2019-09-10 16:24:22 +0200

    OBPIH-2349 Make fields on last page of SM not editable after receiving shipment

--- grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/StockMovementService.groovy
@@ -301,10 +301,6 @@ class StockMovementService {
 
     void clearPicklist(StockMovementItem stockMovementItem) {
         RequisitionItem requisitionItem = RequisitionItem.get(stockMovementItem.id)
-        clearPicklist(requisitionItem)
-    }
-
-    void clearPicklist(RequisitionItem requisitionItem) {
         if (requisitionItem.modificationItem) {
             requisitionItem = requisitionItem.modificationItem
         }
@@ -392,19 +388,17 @@ class StockMovementService {
         }
     }
 
-    void createPicklist(StockMovementItem stockMovementItem) {
-        log.info "Create picklist for stock movement item ${stockMovementItem.toJson()}"
-
-        RequisitionItem requisitionItem = RequisitionItem.get(stockMovementItem.id)
-        createPicklist(requisitionItem)
-    }
-
     /**
      * Create an automated picklist for the given stock movement item.
      *
      * @param id
      */
-    void createPicklist(RequisitionItem requisitionItem) {
+    void createPicklist(StockMovementItem stockMovementItem) {
+
+        log.info "Create picklist for stock movement item ${stockMovementItem.toJson()}"
+
+        // This is kind of a hack, but it's the only way I could figure out how to get the origin field
+        RequisitionItem requisitionItem = RequisitionItem.get(stockMovementItem.id)
         Product product = requisitionItem.product
         Location location = requisitionItem?.requisition?.origin
         Integer quantityRequired = requisitionItem?.calculateQuantityRequired()
@@ -418,9 +412,9 @@ class StockMovementService {
             List<SuggestedItem> suggestedItems = getSuggestedItems(availableItems, quantityRequired)
             log.info "Suggested items " + suggestedItems
             if (suggestedItems) {
-                clearPicklist(requisitionItem)
+                clearPicklist(stockMovementItem)
                 for (SuggestedItem suggestedItem : suggestedItems) {
-                    createOrUpdatePicklistItem(requisitionItem,
+                    createOrUpdatePicklistItem(stockMovementItem,
                             null,
                             suggestedItem.inventoryItem,
                             suggestedItem.binLocation,
@@ -437,13 +431,6 @@ class StockMovementService {
                                     Integer quantity, String reasonCode, String comment) {
 
         RequisitionItem requisitionItem = RequisitionItem.get(stockMovementItem.id)
-        createOrUpdatePicklistItem(requisitionItem, picklistItem, inventoryItem, binLocation, quantity, reasonCode, comment)
-    }
-
-    void createOrUpdatePicklistItem(RequisitionItem requisitionItem, PicklistItem picklistItem,
-                                    InventoryItem inventoryItem, Location binLocation,
-                                    Integer quantity, String reasonCode, String comment) {
-
         Requisition requisition = requisitionItem.requisition
 
         Picklist picklist = Picklist.findByRequisition(requisition)
@@ -480,7 +467,7 @@ class StockMovementService {
             picklistItem.quantity = quantity
             picklistItem.reasonCode = reasonCode
             picklistItem.comment = comment
-            picklistItem.sortOrder = requisitionItem.orderIndex
+            picklistItem.sortOrder = stockMovementItem.sortOrder
         }
         picklist.save(flush: true)
     }
@@ -808,10 +795,6 @@ class StockMovementService {
      */
     PickPageItem buildPickPageItem(RequisitionItem requisitionItem, Integer sortOrder) {
 
-        if (!requisitionItem.picklistItems || (requisitionItem.picklistItems && requisitionItem.totalQuantityPicked() != requisitionItem.quantity &&
-                !requisitionItem.picklistItems.reasonCode)) {
-            createPicklist(requisitionItem)
-        }
         PickPageItem pickPageItem = new PickPageItem(requisitionItem: requisitionItem,
                 picklistItems: requisitionItem.picklistItems)
         Location location = requisitionItem?.requisition?.origin
