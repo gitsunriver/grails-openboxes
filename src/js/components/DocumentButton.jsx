cfb39675173057b0804f07ad51805bfd2ffedb02
gitsunriver <superdeveloper0217@gmail.com>
2019-09-16 14:01:36 +0200

    OBPIH-2333 Improve picklist formatting

--- src/js/components/DocumentButton.jsx
+++ src/js/components/DocumentButton.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function handleClick(onClick, event, link) {
-  event.preventDefault();
-  const newWindow = window.open('', '_blank');
-  onClick()
-    .then(() => { newWindow.location.href = link; });
-}
-
 const DocumentButton = ({
-  buttonIcon, buttonTitle, link, target, disabled, onClick,
+  buttonIcon, buttonTitle, link, target, disabled,
 }) => (
   <a
     href={link}
     className={`py-1 mb-1 btn btn-outline-secondary ${disabled ? 'disabled' : ''}`}
     target={target}
     rel="noopener noreferrer"
-    onClick={event => handleClick(onClick, event, link)}
   >
     <span><i className={`pr-2 fa ${buttonIcon}`} />{buttonTitle}</span>
   </a>
@@ -34,7 +26,6 @@ DocumentButton.propTypes = {
   /** Target attribute, that specifies where to open the linked document (_blank by default) */
   target: PropTypes.string,
   disabled: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
 
 DocumentButton.defaultProps = {
