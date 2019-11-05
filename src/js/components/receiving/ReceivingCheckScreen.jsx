d9d1b22ec11a113ee4c7fe13fd82001d03aa9b8c
gitsunriver <superdeveloper0217@gmail.com>
2019-11-05 10:04:38 +0100

    OBPIH-2336 Add validation for pack level 2 on pack page

--- src/js/components/receiving/ReceivingCheckScreen.jsx
+++ src/js/components/receiving/ReceivingCheckScreen.jsx
@@ -81,7 +81,7 @@ const FIELDS = {
       'container.name': {
         fieldKey: '',
         type: params => (!params.subfield ? <LabelField {...params} /> : null),
-        label: 'react.partialReceiving.packLevel2.label',
+        label: 'react.partialReceiving.box.label',
         defaultMessage: 'Pack level 2',
         flexWidth: '1',
         attributes: {
