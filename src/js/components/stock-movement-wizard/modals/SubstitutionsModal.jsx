598486855c65282c05961e356799f6580718e227
gitsunriver <superdeveloper0217@gmail.com>
2019-12-17 18:18:01 +0100

    OBPIH-2545 Fix doubled substitution reason code on request details tab

--- src/js/components/stock-movement-wizard/modals/SubstitutionsModal.jsx
+++ src/js/components/stock-movement-wizard/modals/SubstitutionsModal.jsx
@@ -227,6 +227,7 @@ class SubstitutionsModal extends Component {
     this.setState({
       formValues: {
         substitutions,
+        reasonCode: originalItem ? '' : 'SUBSTITUTION',
       },
       originalItem,
     });
@@ -255,7 +256,7 @@ class SubstitutionsModal extends Component {
       substitutionItems: _.map(substitutions, sub => ({
         'newProduct.id': sub.product.id,
         newQuantity: sub.quantitySelected,
-        reasonCode: `SUBSTITUTION${values.reasonCode ? ` (${values.reasonCode})` : ''}`,
+        reasonCode: values.reasonCode === 'SUBSTITUTION' ? values.reasonCode : `SUBSTITUTION${values.reasonCode ? ` (${values.reasonCode})` : ''}`,
         sortOrder: originalItem.sortOrder,
       })),
     };
