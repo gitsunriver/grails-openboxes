9e82cf8cc9da30c54a52033ca0c02aa077ec1a19
gitsunriver <superdeveloper0217@gmail.com>
2020-03-16 15:59:32 +0100

    OBPIH-2724 Fix resfresh and substitution issues on edit page

--- src/js/components/stock-movement-wizard/PickPage.jsx
+++ src/js/components/stock-movement-wizard/PickPage.jsx
@@ -209,13 +209,13 @@ class PickPage extends Component {
     this.setState({
       values: {
         ...this.state.values,
-        pickPageItems: this.props.isPaginated ? _.uniqBy(_.concat(
+        pickPageItems: this.props.isPaginated ? _.uniq(_.concat(
           this.state.values.pickPageItems,
           _.map(
             parseResponse(data),
             item => this.checkForInitialPicksChanges(item),
           ),
-        ), 'requisitionItem.id') : _.map(
+        )) : _.map(
           parseResponse(data),
           item => this.checkForInitialPicksChanges(item),
         ),
