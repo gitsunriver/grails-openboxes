65b4ab60fe08245eec000899bca8dd9bbd53fb98
gitsunriver <superdeveloper0217@gmail.com>
2020-08-17 11:01:24 +0200

    OBPIH-3097 Filter out rows with quantity = 0 on inventory template

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -100,11 +100,6 @@ class UrlMappings {
             action = [GET: "getStockMovementItems"]
         }
 
-        "/api/stockMovements/$id/substitutionItems"(parseRequest: true) {
-            controller = "stockMovementItemApi"
-            action = [GET: "getSubstitutionItems"]
-        }
-
         // Stock Movement API
 
         "/api/stockMovements/$id/removeAllItems"(parseRequest: true) {
