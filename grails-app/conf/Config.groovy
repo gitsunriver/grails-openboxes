9aaeed147160da1155745b3479b98d7eaf25381a
gitsunriver <superdeveloper0217@gmail.com>
2020-09-16 17:41:54 +0200

    OBPIH-3165 Add missing error for request type in request stock movement workflow

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -389,11 +389,30 @@ openboxes {
     tablero {
         enabled = true
         configurations {
-            personal    = "My Dashboard"
-            warehouse   = "Warehouse Management"
-            inventory   = "Inventory Management"
-            transaction = "Transaction History"
-            fillRate    = "Fill Rate"
+            personal {
+                name = "My Dashboard"
+                filters {}
+            }
+            warehouse {
+                name = "Warehouse Management"
+                filters {}
+            }
+            inventory {
+                name = "Inventory Management"
+                filters {}
+            }
+            transaction {
+                name = "Transaction Management"
+                filters {}
+            }
+            fillRate {
+                name = "Fill Rate" 
+                filters {
+                    category {
+                        endpoint = "/${appName}/categoryApi/list"
+                    }
+                }
+            }
         }
         endpoints {
             number {
@@ -609,6 +628,21 @@ openboxes {
                     }
                     order = 12
                 }
+                stockOutLastMonth {
+                    enabled = true
+                    endpoint = "/${appName}/apitablero/getStockOutLastMonth"
+                    archived = ['personal', 'warehouse', 'inventory', 'fillRate']
+                    legend = true
+                    datalabel = true
+                    order = 13
+                    colors {
+                        labels {
+                            success = ["Never"]
+                            warning = ["Stocked out <1 week", "Stocked out 1-2 weeks", "Stocked out 2-3 weeks"]
+                            error   = ["Stocked out 3-4 weeks"]
+                        }
+                    }
+                }
             }
         }
     }
@@ -719,8 +753,8 @@ openboxes.hotjar.hjsv = 6
 //openboxes.feedback.enabled = false
 
 // Forecasting feature
-openboxes.forecasting.enabled = true
-openboxes.forecasting.demandPeriod = 365
+openboxes.forecasting.enabled = false
+openboxes.forecasting.demandPeriod = 180
 
 // Bill of Materials feature
 openboxes.bom.enabled = false
