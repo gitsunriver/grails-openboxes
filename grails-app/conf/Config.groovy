b225a32878946b0274c95a5f41c75f160b5d2e40
gitsunriver <superdeveloper0217@gmail.com>
2020-12-10 14:33:26 +0100

    OBPIH-3421 Fix months difference calculation on request report

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -470,12 +470,6 @@ openboxes {
                     archived = ['personal', 'warehouse', 'inventory']
                     order = 9
                 }
-                openStockRequests {
-                    enabled = true
-                    endpoint = "/${appName}/apitablero/getOpenStockRequests"
-                    archived = ['personal', 'warehouse', 'transaction', 'fillRate']
-                    order = 10
-                }
             }
             graph {
                 inventorySummary {
@@ -850,6 +844,13 @@ openboxes.mail.errors.recipients = ["errors@openboxes.com"]
 
 // Barcode scanner (disabled by default)
 openboxes.scannerDetection.enabled = false
+
+// Print barcode labels via USB
+openboxes.barcode.printer.name = "printer-thermalprinter"
+
+// Print barcode labels via RAW
+openboxes.barcode.printer.ipAddress = "127.0.0.1"
+openboxes.barcode.printer.port = 9100
 
 // Default delay and min length for typeahead components
 openboxes.typeahead.delay = 300
