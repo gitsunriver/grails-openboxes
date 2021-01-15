13b4f37ce7aef6267235a8d2fa65c5b15767399f
gitsunriver <superdeveloper0217@gmail.com>
2021-01-15 16:36:01 +0100

    OBPIH-3505 Fix setting maximum height on tables

--- grails-app/services/org/pih/warehouse/shipping/CombinedShipmentService.groovy
+++ grails-app/services/org/pih/warehouse/shipping/CombinedShipmentService.groovy
@@ -44,11 +44,9 @@ class CombinedShipmentService {
                     'lotNumber',
                     'expiry',
                     'quantityToShip',
-                    'unitOfMeasure',
                     'palletName', // pack level 1
                     'boxName', // pack level 2]
                     'recipient',
-                    'budgetCode',
             ]
             orderItems = csvMapReader.toList()
 
