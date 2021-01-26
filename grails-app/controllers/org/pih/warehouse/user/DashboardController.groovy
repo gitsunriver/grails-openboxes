4ddfe0ae8b102921bf9646b9c1721ac6155335dc
gitsunriver <superdeveloper0217@gmail.com>
2021-01-26 11:22:28 +0100

    OBPIH-3326 Fix issues with managing inventory item

--- grails-app/controllers/org/pih/warehouse/user/DashboardController.groovy
+++ grails-app/controllers/org/pih/warehouse/user/DashboardController.groovy
@@ -78,7 +78,11 @@ class DashboardController {
 
         def shipment = Shipment.findByShipmentNumber(params.searchTerms)
         if (shipment) {
-            redirect(controller: "stockMovement", action: "show", id: shipment.id)
+            if (shipment?.isStockMovement()) {
+                redirect(controller: "stockMovement", action: "show", id: shipment?.requisition?.id)
+            } else {
+                redirect(controller: "shipment", action: "showDetails", id: shipment.id)
+            }
             return
         }
 
