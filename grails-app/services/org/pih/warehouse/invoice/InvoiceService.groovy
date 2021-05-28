025aa90a6cbeb560cfa6408e7915f2f646155abf
gitsunriver <superdeveloper0217@gmail.com>
2021-05-28 12:55:59 +0200

    OBAM-117 Distinguish request from depot for PUSH and PULL stocklist replenishment type

--- grails-app/services/org/pih/warehouse/invoice/InvoiceService.groovy
+++ grails-app/services/org/pih/warehouse/invoice/InvoiceService.groovy
@@ -274,7 +274,7 @@ class InvoiceService {
         invoice.invoiceNumber = identifierService.generateInvoiceIdentifier()
         invoice.name = order.name
         invoice.description = order.description
-        invoice.partyFrom = order.destinationParty
+        invoice.partyFrom = order.destination.organization
         invoice.party = order.origin.organization
         invoice.dateInvoiced = LocalDate.now().toDate()
         invoice.currencyUom = UnitOfMeasure.findByCode(order.currencyCode)
