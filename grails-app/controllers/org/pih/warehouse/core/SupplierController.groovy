17793ad4c99b257758a06661eac7e5f500bcd7ad
gitsunriver <superdeveloper0217@gmail.com>
2021-05-28 10:56:41 +0200

    OBAM-223 Add zone to Location

--- grails-app/controllers/org/pih/warehouse/core/SupplierController.groovy
+++ grails-app/controllers/org/pih/warehouse/core/SupplierController.groovy
@@ -12,7 +12,6 @@ package org.pih.warehouse.core
 class SupplierController {
 
     def locationService
-    def documentService
 
     def list = {
         params.max = Math.min(params.max ? params.int('max') : 10, 100)
@@ -21,13 +20,6 @@ class SupplierController {
         def suppliers = locationService.getSuppliers(params.q, params.max, params.offset as int)
 
         [suppliers: suppliers, suppliersTotal: suppliers.totalCount]
-    }
-
-    def show = {
-        Organization supplier = Organization.get(params.id)
-        List<Document> documents = documentService.getAllDocumentsBySupplierOrganization(supplier)
-
-        [supplier: supplier, documents: documents]
     }
 
 }
