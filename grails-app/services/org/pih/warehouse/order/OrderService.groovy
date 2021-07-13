3c82c2a84661d443fe4239c72f23d0ac8761e58d
gitsunriver <superdeveloper0217@gmail.com>
2021-07-13 11:18:11 +0200

    OBPIH-3942 Remove purchase order specific fields from putaway

--- grails-app/services/org/pih/warehouse/order/OrderService.groovy
+++ grails-app/services/org/pih/warehouse/order/OrderService.groovy
@@ -789,12 +789,10 @@ class OrderService {
         }
 
         orderItems.each { orderItem ->
-            if (orderItem.unitOfMeasure) {
-                String[] uomParts = orderItem.unitOfMeasure.split("/")
-                def quantityUom = (int)Double.parseDouble(uomParts[1])
-                orderItem.unitOfMeasure = "${uomParts[0]}/${quantityUom}"
-            }
-            orderItem.unitPrice = orderItem.unitPrice ? new BigDecimal(orderItem.unitPrice).setScale(4, RoundingMode.FLOOR).toString() : ''
+            String[] uomParts = orderItem.unitOfMeasure.split("/")
+            def quantityUom = (int)Double.parseDouble(uomParts[1])
+            orderItem.unitOfMeasure = "${uomParts[0]}/${quantityUom}"
+            orderItem.unitPrice = new BigDecimal(orderItem.unitPrice).setScale(4, RoundingMode.FLOOR).toString()
         }
 
         return orderItems
