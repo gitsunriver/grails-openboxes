8c5bad4711121d2875082579450ced12e9e61c29
gitsunriver <superdeveloper0217@gmail.com>
2021-07-27 05:51:55 +0200

    OBPIH-3945 Use edit_page_item view to build single item

--- grails-app/services/org/pih/warehouse/inventory/InventoryService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/InventoryService.groovy
@@ -1218,17 +1218,15 @@ class InventoryService implements ApplicationContextAware {
     }
 
     Integer getQuantityAvailableToPromise(Product product, Location location) {
-        def productAvailability = ProductAvailability.createCriteria().get {
+        def productAvailability = ProductAvailability.createCriteria().list {
             projections {
-                sum("quantityAvailableToPromise")
+                sum("quantityAvailableToPromise", "quantityAvailableToPromise")
             }
             eq("location", location)
             eq("product", product)
-            // Filter out negative quantity available to promise (in a case when a record was picked and then recalled)
-            ge("quantityAvailableToPromise", 0)
         }
 
-        return productAvailability ?: 0
+        return productAvailability ? productAvailability.get(0) : 0
     }
 
 
