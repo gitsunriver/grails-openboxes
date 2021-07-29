a6518d2eb59dcbac0ba0048ac5f58e3c60e97a7f
gitsunriver <superdeveloper0217@gmail.com>
2021-07-29 12:02:44 +0200

    Minor changes

--- grails-app/services/org/pih/warehouse/inventory/InventoryService.groovy
+++ grails-app/services/org/pih/warehouse/inventory/InventoryService.groovy
@@ -1218,17 +1218,15 @@ class InventoryService implements ApplicationContextAware {
     }
 
     Integer getQuantityAvailableToPromise(Product product, Location location) {
-        def productAvailability = ProductAvailability.createCriteria().get {
+        def productAvailability = ProductAvailability.createCriteria().list {
             projections {
-                sum("quantityAvailableToPromise")
+                sum("quantityAvailableToPromise", "quantityAvailableToPromise")
             }
             eq("location", location)
             eq("product", product)
-            // Filter out negative quantity available to promise (in a case when a record was picked and then recalled)
-            ge("quantityAvailableToPromise", 0)
         }
 
-        return productAvailability ?: 0
+        return productAvailability ? productAvailability.get(0) : 0
     }
 
 
@@ -2721,7 +2719,7 @@ class InventoryService implements ApplicationContextAware {
         command.data.eachWithIndex { row, index ->
             println "${index}: ${row}"
             // ignore a line if physical qoh is empty
-            if (!row.quantity) {
+            if (row.quantity == null) {
                 return
             }
             def transactionEntry = new TransactionEntry()
